c9780d78c333e0b81a38071348695eb6f2d9c4c1
aforashwin <ashwinbalaji454560@gmail.com>
2026-07-28 20:20:56 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,12 @@
+# print("welcome to band name generator")
+# a = input("what is the name of the city you group up in?\n")
+# b=a
+# c = input("what is your pet name?\n")
+# d=c
+# print("your favorite band name is " + b + " " + c)
+
+
+print("welcome to band name generator")
+a = input("what is the name of the city you group up in?\n")
+b = input("what is your pet name?\n")
+print("your favorite band name is " + a + " " + b)
