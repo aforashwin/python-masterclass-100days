e9d7e5ce6d66bb147228f8264361e27fb7e3a115
aforashwin <ashwinbalaji454560@gmail.com>
2026-07-29 19:21:30 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,21 @@
+print("welcome to tip calculator?")
+a=float(input("enter the total amount?"))
+b=float(input("enter the tip amount?"))
+c=int(input("split the tip to be paid by each person?"))
+d=round(b/100*a+a)
+print(f"each individual tip is { d}")
+
+# print("hello"[0:])
+
+# print(123_345_567)
+
+# print(type(1234))
+# print(type("abc"))
+# print(type(1.12))
+# print(type(True))
+
+# print("no of letters in your name:" , len(input("your name:")))
+
+# print("no of letters in your name:" + str(len(input("your name:"))))
+
+# print(3 + 3 + 3 - 3 - 3)
